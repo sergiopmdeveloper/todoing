db82fd5e0f926c1c5297b11813c0e763e52a0458
Sergio <sergio.pm.developer@gmail.com>
2024-12-05 01:14:44 +0000

    ops: initialize project skeleton

---
+++ README.md
@@ -0,0 +1,3 @@
+# Todoing
+
+A sleek and simple task manager built to keep you productive 📝
---
+++ app/routes/home.tsx
@@ -0,0 +1,29 @@
+import type { Route } from './+types/home';
+
+/**
+ * Page metadata.
+ */
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: 'Todoing' },
+    {
+      name: 'description',
+      content:
+        'A sleek and simple task manager built to keep you productive 📝',
+    },
+  ];
+}
+
+/**
+ * Home page.
+ */
+export default function Home() {
+  return (
+    <main>
+      <section>
+        <h1 className="flex text-xl font-bold">Todoing</h1>
+        <p>A sleek and simple task manager built to keep you productive 📝</p>
+      </section>
+    </main>
+  );
+}
