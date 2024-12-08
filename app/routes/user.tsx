4c27ad6474bf92604d603b02a4c91d356887afe2
Sergio <sergio.pm.developer@gmail.com>
2024-12-08 14:35:33 +0000

    feat: implement user info update with validation and error handling

--- app/routes/user.tsx
+++ app/routes/user.tsx
@@ -3,7 +3,9 @@ import { Input } from '@nextui-org/input';
 import { Link } from '@nextui-org/link';
 import jwt from 'jsonwebtoken';
 import { useState } from 'react';
-import { redirect } from 'react-router';
+import { redirect, useFetcher } from 'react-router';
+import { z } from 'zod';
+import FieldError from '~/components/field-error';
 import { Section } from '~/layouts/section';
 import { session } from '~/utils/cookies.server';
 import { db } from '~/utils/db.server';
@@ -75,14 +77,64 @@ export async function loader({ request, params }: Route.LoaderArgs) {
 }
 
 /**
+ * User page server action.
+ * @param {Route.ActionArgs} args - The action arguments.
+ * @param {Route.ActionArgs} args.request - The incoming request.
+ * @param {Route.ActionArgs["params"]} args.params - The route parameters.
+ */
+export async function action({ request, params }: Route.ActionArgs) {
+  const formData = await request.formData();
+  const name = formData.get('name') as string;
+  const email = formData.get('email') as string;
+
+  const updateUserInfoSchema = z.object({
+    name: z
+      .string()
+      .max(50, 'Cannot exceed 50 characters')
+      .regex(
+        /^[A-Za-zÀ-ÖØ-öø-ÿ' -]+$/,
+        'Can only include letters, spaces, hyphens and apostrophes'
+      ),
+    email: z.string().min(1, 'Required').email('Invalid email'),
+  });
+
+  const fieldValidation = updateUserInfoSchema.safeParse({ name, email });
+
+  if (!fieldValidation.success) {
+    return {
+      fieldErrors: fieldValidation.error.flatten().fieldErrors,
+    };
+  }
+
+  await db.user.update({
+    where: {
+      id: params.userId,
+    },
+    data: {
+      name,
+      email,
+    },
+  });
+
+  return {
+    fieldErrors: {},
+  };
+}
+
+/**
  * User page.
  * @param {Route.ComponentProps} props - The page props.
  * @param {Route.ComponentProps["loaderData"]} props.loaderData - The loader data.
  */
 export default function User({ loaderData }: Route.ComponentProps) {
+  const fetcher = useFetcher<typeof action>();
   const { name, email } = loaderData && 'name' in loaderData ? loaderData : {};
   const [actualName, setActualName] = useState(name);
   const [actualEmail, setActualEmail] = useState(email);
+
+  const nameErrors = fetcher.data?.fieldErrors.name;
+  const emailErrors = fetcher.data?.fieldErrors.email;
+  const submitting = fetcher.state !== 'idle';
 
   const nameHasNotChanged = name === actualName;
   const emailHasNotChanged = email === actualEmail;
@@ -98,7 +150,7 @@ export default function User({ loaderData }: Route.ComponentProps) {
       <Section>
         <h2 className="mb-4 text-xl">Account details</h2>
 
-        <form>
+        <fetcher.Form method="post">
           <div className="mb-4 space-y-4">
             <Input
               id="name"
@@ -108,6 +160,10 @@ export default function User({ loaderData }: Route.ComponentProps) {
               defaultValue={name}
               autoComplete="name"
               label="Name"
+              isInvalid={!!nameErrors}
+              errorMessage={
+                nameErrors && <FieldError>{nameErrors[0]}</FieldError>
+              }
             />
 
             <Input
@@ -118,6 +174,10 @@ export default function User({ loaderData }: Route.ComponentProps) {
               defaultValue={email}
               autoComplete="email"
               label="Email"
+              isInvalid={!!emailErrors}
+              errorMessage={
+                emailErrors && <FieldError>{emailErrors[0]}</FieldError>
+              }
             />
           </div>
 
@@ -132,11 +192,12 @@ export default function User({ loaderData }: Route.ComponentProps) {
             type="submit"
             color="primary"
             isDisabled={nameHasNotChanged && emailHasNotChanged}
+            isLoading={submitting}
             fullWidth
           >
             Save
           </Button>
-        </form>
+        </fetcher.Form>
       </Section>
     </main>
   );
