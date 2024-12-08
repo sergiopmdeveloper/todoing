b853511aab75b0f802cd2f5e0ee54f837ac4b0dc
Sergio <sergio.pm.developer@gmail.com>
2024-12-08 02:20:16 +0000

    style: implement ui of the account details form

--- app/routes/user.tsx
+++ app/routes/user.tsx
@@ -1,7 +1,12 @@
+import { Button } from '@nextui-org/button';
+import { Input } from '@nextui-org/input';
+import { Link } from '@nextui-org/link';
 import jwt from 'jsonwebtoken';
+import { useState } from 'react';
 import { redirect } from 'react-router';
 import { Section } from '~/layouts/section';
 import { session } from '~/utils/cookies.server';
+import { db } from '~/utils/db.server';
 import type { Route } from './+types/user';
 
 /**
@@ -51,8 +56,21 @@ export async function loader({ request, params }: Route.LoaderArgs) {
     return redirect(`/user/${sessionData.sub}`);
   }
 
+  const user = await db.user.findFirst({
+    where: {
+      id: params.userId,
+    },
+  });
+
+  let userData;
+
+  if (user) {
+    const { id, password, ...userWithoutPassword } = user;
+    userData = userWithoutPassword;
+  }
+
   return {
-    userId: params.userId,
+    ...userData,
   };
 }
 
@@ -62,13 +80,63 @@ export async function loader({ request, params }: Route.LoaderArgs) {
  * @param {Route.ComponentProps["loaderData"]} props.loaderData - The loader data.
  */
 export default function User({ loaderData }: Route.ComponentProps) {
-  const { userId } = loaderData && 'userId' in loaderData ? loaderData : {};
+  const { name, email } = loaderData && 'name' in loaderData ? loaderData : {};
+  const [actualName, setActualName] = useState(name);
+  const [actualEmail, setActualEmail] = useState(email);
+
+  const nameHasNotChanged = name === actualName;
+  const emailHasNotChanged = email === actualEmail;
 
   return (
     <main>
       <Section>
-        <h1 className="flex text-3xl font-bold">User page</h1>
-        <p>{userId}</p>
+        <h1 className="text-2xl">
+          Welcome back <strong>{name}</strong>
+        </h1>
+      </Section>
+
+      <Section>
+        <h2 className="mb-4 text-xl">Account details</h2>
+
+        <form>
+          <div className="mb-4 space-y-4">
+            <Input
+              id="name"
+              name="name"
+              placeholder="Enter your name..."
+              onChange={(event) => setActualName(event.target.value)}
+              defaultValue={name}
+              autoComplete="name"
+              label="Name"
+            />
+
+            <Input
+              id="email"
+              name="email"
+              placeholder="Enter your email..."
+              onChange={(event) => setActualEmail(event.target.value)}
+              defaultValue={email}
+              autoComplete="email"
+              label="Email"
+            />
+          </div>
+
+          <p className="mb-4 text-small">
+            Do you want to change your password?{' '}
+            <Link href="#" size="sm" underline="always">
+              Change password
+            </Link>
+          </p>
+
+          <Button
+            type="submit"
+            color="primary"
+            isDisabled={nameHasNotChanged && emailHasNotChanged}
+            fullWidth
+          >
+            Save
+          </Button>
+        </form>
       </Section>
     </main>
   );
