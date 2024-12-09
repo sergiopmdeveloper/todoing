6a088970c555e5a9dd25d760faa65de4012f1118
Sergio <sergio.pm.developer@gmail.com>
2024-12-09 00:31:57 +0000

    ops: add todos seed

---
+++ prisma/seeds/todos.ts
@@ -0,0 +1,26 @@
+export const todos = [
+  {
+    id: 'todo1',
+    name: 'Buy groceries',
+    description: 'Milk, Bread, Cheese, Eggs',
+    priority: 1,
+    deadline: new Date('2025-12-01T00:00:00Z'),
+    userId: 'cm4cp1yl80000qcc3zsynss4s',
+  },
+  {
+    id: 'todo2',
+    name: 'Complete project report',
+    description: 'Finish the final draft of the project report',
+    priority: 2,
+    deadline: new Date('2025-12-05T00:00:00Z'),
+    userId: 'cm4cp1yl80000qcc3zsynss4s',
+  },
+  {
+    id: 'todo3',
+    name: 'Workout',
+    description: 'Go to the gym for a workout session',
+    priority: 3,
+    deadline: new Date('2025-12-02T00:00:00Z'),
+    userId: 'cm4cp1yl80000qcc3zsynss4s',
+  },
+];
